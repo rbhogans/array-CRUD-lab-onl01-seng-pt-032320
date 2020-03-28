b042a1208f03060cb7368108af30c2ae06accfc4
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-28 05:26:13 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,9 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("element")
+  array=girl
+  element=boy
+  array.unshift("boy")
   
 end
 
