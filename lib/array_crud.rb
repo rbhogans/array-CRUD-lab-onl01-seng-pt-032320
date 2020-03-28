49fb6a0f566236f6373e319a14cda2ffedfbc318
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-28 05:05:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-       add_element_to_end_of_array << "kenny"
+       array << element
     puts add_element_to_end_of_array.inspect
    
 end
