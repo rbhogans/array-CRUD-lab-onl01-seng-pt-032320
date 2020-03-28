6c41584f0f0170d155707dd4276597b3f1ce1692
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-28 05:54:06 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,19 +14,19 @@ end
 
 def add_element_to_start_of_array(array, element)
   
-  array.unshift(element)
+       array.unshift(element)
   
 end
 
 def remove_element_from_end_of_array(array)
   
-  array.pop
+          array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
-  array.shift
+   
+          array.shift
   
 end
 
