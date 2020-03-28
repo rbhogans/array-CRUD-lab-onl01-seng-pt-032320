ae4f43f40155dff93f052ba85fc903ed37381f3b
Rabiah Hogans <hogansrabiah@gmail.com>
2020-03-28 04:44:07 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -14,7 +14,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+   add_element_to_start_of_array(array, element)
+   add_element_to_start_of_array = ["mom","dad", "wali", "osman"]
+   add_element_to_start_of_array << "kenny"
+   puts add_element_to_start_of_array.inspect
+   
 end
 
 def remove_element_from_end_of_array(array)
